87899fe9aee2f432c401d40d3d0eb7d561a43215
Yuval Shalgi <yuvals@perfectomobile.com>
2025-08-01 13:27:52 +0300

    get simple response

--- src/main/java/com/moshecode/todo1/controller/ToDoController.java
+++ src/main/java/com/moshecode/todo1/controller/ToDoController.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController //question -> why did controller not work?
 @RequiredArgsConstructor
 public class ToDoController {
     private final ToDo toDo;
